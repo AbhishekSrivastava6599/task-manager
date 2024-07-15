e544d8f8261d6611adb74a46472d013b82ccddd4
AbhishekSrivastava6599 <73079478+AbhishekSrivastava6599@users.noreply.github.com>
2024-07-16 02:42:57 +0530

    added POST /tasks endpoint

--- app.js
+++ app.js
@@ -55,6 +55,37 @@ app.get('/tasks/:id', (req, res) => {
   }
 });
 
+// POST /tasks: Create a new task
+app.post('/tasks', (req, res) => {
+  try {
+    console.log(`--POST /tasks endpoint called`);
+    // Validate request body
+    const { title, description, completed } = req.body;
+    if (!title || !description || typeof completed !== 'boolean') {
+      return res.status(400).json({ error: 'Invalid request. Please provide valid task details.' });
+    }
+
+    // Generate a new task ID
+    const newTaskId = tasksArray.length + 1;
+
+    const newTask = {
+      id: newTaskId,
+      title,
+      description,
+      completed,
+    };
+
+    // Add the new task to the tasks array
+    tasksArray.push(newTask);
+    // console.log(tasksArray)
+    res.status(201).json(newTask);
+  } catch (error) {
+    console.error('Error handling request:', error.message);
+    res.status(500).json({ error: 'Internal server error.' });
+  }
+});
+
+
 app.listen(port, (err) => {
     if (err) {
         return console.log('Something bad happened', err);
